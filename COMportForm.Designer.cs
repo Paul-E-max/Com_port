ee04e973b9f63954e4a7967699abd37d6eb1a672
Michael Dodd <Michael.Dodd@fosterfreeman.com>
2022-05-20 11:09:53 +0100

    First submission is V1.00.03

---
+++ COMportForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace COMport
+{
+    partial class COMportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(COMportForm));
+            this.CommsTextBox = new System.Windows.Forms.TextBox();
+            this.BaudComboBox = new System.Windows.Forms.ComboBox();
+            this.COMportComboBox = new System.Windows.Forms.ComboBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clearSreenButton = new System.Windows.Forms.Button();
+            this.VersionComboBox = new System.Windows.Forms.ComboBox();
+            this.SerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CommsTextBox
+            // 
+            this.CommsTextBox.AllowDrop = true;
+            this.CommsTextBox.BackColor = System.Drawing.SystemColors.MenuText;
+            this.CommsTextBox.CausesValidation = false;
+            this.CommsTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CommsTextBox.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CommsTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.CommsTextBox.Location = new System.Drawing.Point(0, 0);
+            this.CommsTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CommsTextBox.MaxLength = 250000;
+            this.CommsTextBox.Multiline = true;
+            this.CommsTextBox.Name = "CommsTextBox";
+            this.CommsTextBox.ReadOnly = true;
+            this.CommsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.CommsTextBox.Size = new System.Drawing.Size(921, 554);
+            this.CommsTextBox.TabIndex = 3;
+            this.CommsTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CommsTextBox_KeyPress);
+            // 
+            // BaudComboBox
+            // 
+            this.BaudComboBox.FormattingEnabled = true;
+            this.BaudComboBox.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "900",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.BaudComboBox.Location = new System.Drawing.Point(308, 6);
+            this.BaudComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BaudComboBox.Name = "BaudComboBox";
+            this.BaudComboBox.Size = new System.Drawing.Size(141, 21);
+            this.BaudComboBox.TabIndex = 1;
+            this.BaudComboBox.Text = "115200";
+            // 
+            // COMportComboBox
+            // 
+            this.COMportComboBox.FormattingEnabled = true;
+            this.COMportComboBox.Location = new System.Drawing.Point(228, 6);
+            this.COMportComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.COMportComboBox.Name = "COMportComboBox";
+            this.COMportComboBox.Size = new System.Drawing.Size(77, 21);
+            this.COMportComboBox.TabIndex = 0;
+            this.COMportComboBox.DropDown += new System.EventHandler(this.COMportComboBox_DropDown);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectButton.Location = new System.Drawing.Point(4, 4);
+            this.ConnectButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(97, 24);
+            this.ConnectButton.TabIndex = 2;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.clearSreenButton);
+            this.panel1.Controls.Add(this.VersionComboBox);
+            this.panel1.Controls.Add(this.BaudComboBox);
+            this.panel1.Controls.Add(this.COMportComboBox);
+            this.panel1.Controls.Add(this.ConnectButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(921, 34);
+            this.panel1.TabIndex = 4;
+            // 
+            // clearSreenButton
+            // 
+            this.clearSreenButton.Location = new System.Drawing.Point(823, 6);
+            this.clearSreenButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.clearSreenButton.Name = "clearSreenButton";
+            this.clearSreenButton.Size = new System.Drawing.Size(91, 24);
+            this.clearSreenButton.TabIndex = 4;
+            this.clearSreenButton.Text = "Clear screen";
+            this.clearSreenButton.UseVisualStyleBackColor = true;
+            this.clearSreenButton.Click += new System.EventHandler(this.clearSreenButton_Click);
+            // 
+            // VersionComboBox
+            // 
+            this.VersionComboBox.FormattingEnabled = true;
+            this.VersionComboBox.Items.AddRange(new object[] {
+            "None",
+            "AARI",
+            "DISCOVER",
+            "PDi"});
+            this.VersionComboBox.Location = new System.Drawing.Point(103, 6);
+            this.VersionComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.VersionComboBox.Name = "VersionComboBox";
+            this.VersionComboBox.Size = new System.Drawing.Size(122, 21);
+            this.VersionComboBox.TabIndex = 3;
+            this.VersionComboBox.Text = "DISCOVER";
+            // 
+            // SerialPort
+            // 
+            this.SerialPort.DtrEnable = true;
+            this.SerialPort.RtsEnable = true;
+            this.SerialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort_DataReceived);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Controls.Add(this.CommsTextBox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 34);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(921, 554);
+            this.panel2.TabIndex = 5;
+            // 
+            // COMportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(921, 588);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "COMportForm";
+            this.Text = "COM port";
+            this.Load += new System.EventHandler(this.COMportForm_Load);
+            this.Shown += new System.EventHandler(this.COMportForm_Shown);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CommsTextBox;
+        private System.Windows.Forms.ComboBox BaudComboBox;
+        private System.Windows.Forms.ComboBox COMportComboBox;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.IO.Ports.SerialPort SerialPort;
+        private System.Windows.Forms.ComboBox VersionComboBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button clearSreenButton;
+    }
+}
+
---
+++ COMportForm.cs
@@ -0,0 +1,432 @@
+﻿// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// @File:       COMportForm.cs
+// @Project:    DISCOVER_UTIL\COMport
+// @Author:     Foster & Freeman Ltd - Michael Dodd
+// @Created:    31.03.2022
+//
+// @Brief:      Simple COM port communications device for use with DISCOVER.
+//
+// @Tools:      Visual Studio 2019, C#
+//
+// @Revision:
+// 28.04.2022-MD Clear screen button added.
+// 06.04.2022-MD Correction to BS when textbox is scrolled!
+// 01.04.2022-MD Handles BS coming from COM port.
+// 31.03.2022-MD Initial version.
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMport
+{
+    public partial class COMportForm : Form
+    {
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // Constants
+        //
+        const string APP_NAME = "COMport";
+        const string VERSION = "V1.00.03";
+        const string FILENAME_CSV = APP_NAME + ".CSV";
+
+        const string CONNECT_LABEL = "Connect";
+        const string SCANNING_LABEL = "Connecting";
+        const string DISCONNECT_LABEL = "Disconnect";
+        
+        const int TIMEOUT_IMMEDIATE = 1;        // Timeout during clearing of buffer.
+        const int TIMEOUT_CLEARS = 20;          // Number of times to attempt clear of buffer.
+        const int TIMEOUT_NORMAL = 200;         // Normal period to wait for TX or RX to complete (use -1 for debugging, gives infinite period).
+        const int TIMEOUT_MAY = 50;             // If not sure that a response is due.
+        const int INTERVAL_RESPOND = 175;       // Period between TX and looking for RX.
+
+        const byte BS = 0x08;
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// 
+        /// </summary>
+        public COMportForm()
+        {
+            InitializeComponent();
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// This occurs before the user sees any response on the screen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void COMportForm_Load(object sender, EventArgs e)
+        {
+            this.Text = APP_NAME + " - " + VERSION;
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// Once the frame is displayed, it is OK to fill in details.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void COMportForm_Shown(object sender, EventArgs e)
+        {
+            scanForAvailableCOMports();
+            // LoadConfigurationCSV();
+            //
+            // If scan found any COM ports and CSV did not define a valid port, then
+            // just display the first item in the list.
+            //
+            if (COMportComboBox.Text.Length > 0)
+            {
+                // The CSV file had a COM port that is plugged in, so lets try and connect.
+                //
+                ConnectButton.PerformClick();
+            }
+            else
+            {
+                // Guess the first COM port in the list is the one.
+                //
+                if (COMportComboBox.Items.Count > 0) COMportComboBox.Text = COMportComboBox.Items[0].ToString();
+                //
+                // If there is only one of them, try to connect to it, otherwise will have to let user select it
+                // manually.
+                //
+                if (1 == COMportComboBox.Items.Count) ConnectButton.PerformClick();
+            }
+
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// Connecting to the selected COM port (or scan if none selected).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            string versionIs;
+
+            if (CONNECT_LABEL == ConnectButton.Text)
+            {
+                // CONNECTING TO COM PORT
+                // ----------------------
+                //
+                if (0 == COMportComboBox.Text.Length)
+                {
+                    // Nothing select at the time of hitting connect, so rescan now.
+                    //
+                    scanForAvailableCOMports();
+                    //
+                    // Grab the first available one.
+                    //
+                    if (COMportComboBox.Items.Count > 0) COMportComboBox.Text = COMportComboBox.Items[0].ToString();
+                }
+                if (COMportComboBox.Text.Length > 0)
+                {
+                    ConnectButton.Text = SCANNING_LABEL;
+                    ConnectButton.BackColor = System.Drawing.Color.Yellow;
+                    ConnectButton.Refresh();
+                    //
+                    // Attempt to connect to a COM port . . .
+                    //
+                    if (COMportComboBox.Text.Substring(0, 3) == "COM")
+                    {
+                        try // The following is sensitive to communication errors, and will abandon the task if one occurs.
+                        {
+                            if (SerialPort.IsOpen) SerialPort.Close();
+                            SerialPort.PortName = COMportComboBox.Text;
+                            SerialPort.ReadTimeout = TIMEOUT_NORMAL;
+                            SerialPort.WriteTimeout = TIMEOUT_NORMAL;
+                            SerialPort.BaudRate = Convert.ToInt32(BaudComboBox.Text);
+                            SerialPort.Open();
+                            if (VersionComboBox.Text != "None")
+                            {
+                                serialPortWriteLine("");
+                                serialPortWriteLine("ECHO OFF");
+                                versionIs = serialPortCommandresponse("VERSION");
+                                if( versionIs.StartsWith(VersionComboBox.Text))
+                                {
+                                    ConnectButton.Text = DISCONNECT_LABEL;
+                                    HighLightButton(ConnectButton);
+                                    this.Text = APP_NAME + " - " + VERSION + " ---> " + versionIs;
+                                    COMportComboBox.Enabled = false;
+                                    serialPortWriteLine("ECHO ON");
+                                }
+                            }
+                            else
+                            {
+                                ConnectButton.Text = DISCONNECT_LABEL;
+                                HighLightButton(ConnectButton);
+                                COMportComboBox.Enabled = false;
+                                this.Text = APP_NAME + " - " + VERSION + " connected";
+                            }
+                            CommsTextBox.Focus();
+                        }
+                        catch
+                        {
+                            // Don't do anything with this.
+                        }
+                    }
+                }
+            }
+            else
+            {
+                COMportComboBox.Enabled = true;
+            }
+            if( COMportComboBox.Enabled )
+            {
+                if (SerialPort.IsOpen ) SerialPort.Close();
+                ConnectButton.Text = CONNECT_LABEL;
+                LowLightButton(ConnectButton);
+                this.Text = APP_NAME + " - " + VERSION;
+                COMportComboBox.Enabled = true;
+            }
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// Scan for available COM ports and populate COMportComboBox.
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        ///
+        private void scanForAvailableCOMports()
+        {
+            COMportComboBox.Items.Clear();
+            //
+            foreach (string name in SerialPort.GetPortNames())
+            {
+                COMportComboBox.Items.Add(name);
+            }
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// Clear out any characters hanging around in the ports output buffer.
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        /// 
+        private void serialPortClearbuffer()
+        {
+            SerialPort.ReadTimeout = TIMEOUT_IMMEDIATE;
+            //
+            for (int i = 0; i < TIMEOUT_CLEARS; i++)
+            {
+                try
+                {
+                    SerialPort.ReadLine();
+                }
+                catch
+                {
+                    break; // Nothing immediately available, so done.
+                }
+            }
+            SerialPort.ReadTimeout = TIMEOUT_NORMAL;
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// Write a line to the serial port, ignore response.
+        /// </summary>
+        private void serialPortWriteLine(string command)
+        {
+            serialPortClearbuffer();
+            //
+            SerialPort.WriteLine(command);
+            Thread.Sleep(INTERVAL_RESPOND);
+            //
+            SerialPort.ReadTimeout = TIMEOUT_MAY; // Select a short timeout.
+            try
+            {
+                SerialPort.ReadLine();
+            }
+            catch
+            {
+                // ignore no response, possibly just no echo.
+            }
+            SerialPort.ReadTimeout = TIMEOUT_NORMAL;
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// Read a line from the serial port, but discard any CRLF characters.
+        /// </summary>
+        private string serialPortReadLine()
+        {
+            string response;
+
+            Thread.Sleep(INTERVAL_RESPOND);
+            response = SerialPort.ReadLine();
+            response = response.Replace("\r", "").Replace("\n", "");
+            //
+            return response;
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// Send the command and get a response.
+        /// </summary>
+        /// <param name="command"> to send </param>
+        /// <returns>Responding string</returns>
+        private string serialPortCommandresponse(string command)
+        {
+            string response;
+
+            serialPortClearbuffer(); // Make sure nothing is sitting in the pipeline.
+            //
+            SerialPort.WriteLine(command);
+            response = serialPortReadLine();
+            //
+            return response;
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// High light a button as green with white bold text.
+        /// </summary>
+        /// <param name="highLightButton">The button to be modified.</param>
+        /// <param name="highLightColor">The colour to use, typically green or red.</param>
+        /// <returns></returns>
+        ///
+        private void HighLightButton(Button highLightButton)
+        {
+            highLightButton.BackColor = System.Drawing.Color.LimeGreen;
+            highLightButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            highLightButton.Font = (new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (byte)(0)));
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// Low light a button as red with black regular text.
+        /// </summary>
+        /// <param name="lowLightButton">The button to be modified.</param>
+        /// <returns></returns>
+        ///
+        private void LowLightButton(Button lowLightButton)
+        {
+            lowLightButton.BackColor = System.Drawing.Color.LightCoral;
+            lowLightButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            lowLightButton.Font = (new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (byte)(0)));
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// When ever serial data is received, display it on CommsTextBox.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int i;
+
+            if ( false == COMportComboBox.Enabled )
+            {
+                int readLength = SerialPort.BytesToRead;
+                var inputs = new byte[readLength];
+
+                SerialPort.Read(inputs, 0, readLength);
+#if false
+                for (i = 0; i < readLength; i++)
+                {
+                    if (BS == inputs[i])
+                    {
+                        for( i=0; i<readLength; i++ ) CommsTextBox.AppendText("["+inputs[i].ToString()+"], "); // MJD2022
+                        //
+                        break;
+                    }
+                }
+#endif
+                for ( i=0; i<readLength; i++ )
+                {
+                    if( BS == inputs[i] )
+                    {
+                        if ( i != 0 )
+                        {
+                            // Need to handle the characters in front of the BS.
+                            //
+                            CommsTextBox.AppendText(Encoding.ASCII.GetString(inputs, 0, i));
+                            //
+                            // And force the byte array to have BS as the first character.
+                            //
+                            for (int shift = i; shift < readLength; shift++)
+                            {
+                                inputs[shift - i] = inputs[shift];
+                            }
+                            readLength -= i;
+                            i = 0;
+                        }
+                        // The BS causes the CommsTextBox to loose a character at the end of the existing content.
+                        //
+                        if(CommsTextBox.TextLength > 0) CommsTextBox.Text = CommsTextBox.Text.Substring(0, CommsTextBox.TextLength - 1);
+                        //
+                        for ( int shift=1; shift<readLength; shift++)
+                        {
+                            inputs[shift - 1] = inputs[shift];
+                        }
+                        readLength--;
+                        i--;
+                    }
+                }
+                if( readLength > 0 ) CommsTextBox.AppendText(Encoding.ASCII.GetString(inputs, 0, readLength));
+                //
+                CommsTextBox.SelectionStart = CommsTextBox.Text.Length; // Place the curser at the end of the text.
+                CommsTextBox.ScrollToCaret();
+            }
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// On key being pressed, send it to COM port.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CommsTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var buffer = new byte[1];
+
+            if (false == COMportComboBox.Enabled)
+            {
+                buffer[0] = (byte)e.KeyChar;
+
+                if (buffer[0] < 0x80)
+                {
+                    SerialPort.Write(buffer, 0, 1);
+                }
+            }
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// On clicking COM port combo, rescan for COM ports.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void COMportComboBox_DropDown(object sender, EventArgs e)
+        {
+            scanForAvailableCOMports();
+        }
+
+        /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        /// <summary>
+        /// Clear the screen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void clearSreenButton_Click(object sender, EventArgs e)
+        {
+            CommsTextBox.Text = "";
+            CommsTextBox.Focus();
+        }
+    }
+}
